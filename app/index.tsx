07d0a2ec3caf45f78a2bcdc97d01d1879127bfb9
Henry Leung <henry.leung@edu.sait.ca>
2026-02-27 20:32:06 -0700

    Lab 3 changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,24 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+"use client";
+
 import ToDoForm from "@/components/ToDoForm";
 import ToDoList from "@/components/ToDoList";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+export type Task = {
+  id: string;
+  task: string;
+};
 
 function Index() {
+  const [tasks, setTasks] = useState([
+    { id: "1", task: "Do Laundry" },
+    { id: "2", task: "Go to Gym" },
+    { id: "3", task: "Walk dog" },
+  ]);
   return (
     <View>
-      <ToDoList />
+      <ToDoList tasks={tasks} />
       <ToDoForm />
     </View>
   );
--- components/ToDoList.tsx
+++ components/ToDoList.tsx
@@ -1,24 +1,17 @@
-import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
 import React from "react";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Task } from "../app/index";
 
-const ToDoList = () => {
+const ToDoList = ({ tasks }: { tasks: Task[] }) => {
   return (
     <ScrollView>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Do laundry</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task]}>
-          <Text style={styles.taskText}>Go to gym</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Walk dog</Text>
-        </View>
-      </Pressable>
+      {tasks.map((task) => (
+        <Pressable key={task.id}>
+          <View style={[styles.task, styles.completed]}>
+            <Text style={styles.taskText}>{task.task}</Text>
+          </View>
+        </Pressable>
+      ))}
     </ScrollView>
   );
 };
